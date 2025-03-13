5ce53317ce51a40fb5ddfe2846a95ab4f3093c87
Denys <denysyefimov2003@gmail.com>
2025-03-13 20:07:10 +0200

    Add Id column to PostcodeService export file

--- Services/Services/PostcodeService.cs
+++ Services/Services/PostcodeService.cs
@@ -100,25 +100,27 @@ namespace Services.Services
             using var package = new ExcelPackage();
             var worksheet = package.Workbook.Worksheets.Add("AUP");
 
-            worksheet.Cells[1, 1].Value = "Postcode";
-            worksheet.Cells[1, 2].Value = "City Code";
-            worksheet.Cells[1, 3].Value = "City Name";
-            worksheet.Cells[1, 4].Value = "Region Code";
-            worksheet.Cells[1, 5].Value = "Region Name";
-            worksheet.Cells[1, 6].Value = "District Code";
-            worksheet.Cells[1, 7].Value = "District Name";
+            worksheet.Cells[1, 1].Value = "Id";
+            worksheet.Cells[1, 2].Value = "Postcode";
+            worksheet.Cells[1, 3].Value = "City Code";
+            worksheet.Cells[1, 4].Value = "City Name";
+            worksheet.Cells[1, 5].Value = "Region Code";
+            worksheet.Cells[1, 6].Value = "Region Name";
+            worksheet.Cells[1, 7].Value = "District Code";
+            worksheet.Cells[1, 8].Value = "District Name";
 
             int row = 2;
             foreach (var aup in aupData)
             {
-                worksheet.Cells[row, 1].Value = aup.Postcode;
-                worksheet.Cells[row, 2].Value = aup.CityCode;
-                worksheet.Cells[row, 3].Value = aup.CityName;
-                worksheet.Cells[row, 4].Value = aup.RegionCode;
-                worksheet.Cells[row, 5].Value = aup.RegionName;
-                worksheet.Cells[row, 6].Value = aup.DistrictCode;
-                worksheet.Cells[row, 7].Value = aup.DistrictName;
-                row++;
+                worksheet.Cells[row, 1].Value = aup.Id;
+                worksheet.Cells[row, 2].Value = aup.Postcode;
+                worksheet.Cells[row, 3].Value = aup.CityCode;
+                worksheet.Cells[row, 4].Value = aup.CityName;
+                worksheet.Cells[row, 5].Value = aup.RegionCode;
+                worksheet.Cells[row, 6].Value = aup.RegionName;
+                worksheet.Cells[row, 7].Value = aup.DistrictCode;
+                worksheet.Cells[row, 8].Value = aup.DistrictName;
+                ++row;
             }
 
             var stream = new MemoryStream();
