09b80d11cb26de978c5f12b7c27bb178d2adc366
Denys <denysyefimov2003@gmail.com>
2025-03-13 19:53:30 +0200

    Update PostcodeController with concrete response types

--- NibulonTest/Controllers/PostcodeController.cs
+++ NibulonTest/Controllers/PostcodeController.cs
@@ -1,4 +1,5 @@
-﻿using Common.Models;
+﻿using Common.Dtos;
+using Common.Models;
 using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 
@@ -16,31 +17,31 @@ namespace NibulonTest.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAups([FromQuery]PaginationSettings paginationSettings)
+        public async Task<ActionResult<List<AupDto>>> GetAups([FromQuery]PaginationSettings paginationSettings)
         {
             return Ok(await _postcodeService.GetAupsAsync(paginationSettings));
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPostcodesWithoutCity()
+        public async Task<ActionResult<List<AupDto>>> GetPostcodesWithoutCity()
         {
             return Ok(await _postcodeService.GetPostcodesWithoutCityAsync());
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPostcodesWithoutDistrict()
+        public async Task<ActionResult<List<AupDto>>> GetPostcodesWithoutDistrict()
         {
             return Ok(await _postcodeService.GetPostcodesWithoutDistrictAsync());
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPostcodesWithoutRegion()
+        public async Task<ActionResult<List<AupDto>>> GetPostcodesWithoutRegion()
         {
             return Ok(await _postcodeService.GetPostcodesWithoutRegionAsync());
         }
 
         [HttpPost]
-        public async Task<IActionResult> ImportAupFromExcel(IFormFile file)
+        public async Task<ActionResult> ImportAupFromExcel(IFormFile file)
         {
             if (file == null || file.Length == 0)
             {
@@ -52,7 +53,7 @@ namespace NibulonTest.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> ExportAupToExcel()
+        public async Task<FileContentResult> ExportAupToExcel()
         {
             var fileContent = await _postcodeService.ExportAupToExcelAsync();
             return File(fileContent, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "AUP.xlsx");
